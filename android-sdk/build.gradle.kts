5fff18811d963eb2e87a14b44ed3d5d7d9672dd6
oxdev6 <oxdev6@users.noreply.github.com>
2025-09-22 11:47:34 +0300

    chore: remove empty files and clean Android consumer rules

--- android-sdk/build.gradle.kts
+++ android-sdk/build.gradle.kts
@@ -11,7 +11,6 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 34
-        consumerProguardFiles("consumer-rules.pro")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
